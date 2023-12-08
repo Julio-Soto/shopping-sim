9c8b514dfd77a071dae583ed986354ff8e9cd993
Julio Soto <juliosotodev@gmail.com>
2023-12-07 20:03:22 -0700

    testing funcs in main

--- main.cpp
+++ main.cpp
@@ -1,67 +1,48 @@
+//#include "shopMenu.h"
+#include "Item.h"
+#include "Customer.h"
 #include <iostream>
+    using std::cout;
+    using std::endl;
+    using std::endl;
+    using std::cerr;
+    using std::stoi;
+    using std::stof;
 #include <fstream>
+    using std::ifstream;
+    using std::getline;
 #include <sstream>
+    using std::istringstream;
 #include <string>
+    using std::string;
 
-struct Item {
-    unsigned int code;
-    std::string description;
-    float price;
-    bool per_pound;
-};
 
-int main() {
+int main()
+{
+    // shopMenu();
+    Store myStore{};
+    Store* myStorePtr = &myStore;
+    myStore.stock("inventory-test.txt");
+    int numOfitems = 5;
+    Customer cust(numOfitems,&myStore);
 
-    std::string file = "inventory-test.txt";
-    std::string bar_delimiter = "|";
-    std::string slash_delimiter = "/";
-    int delimeter_pos = 0;
-    std::string code;
+    cust.fillCart();
 
-    // Open the file
-    std::ifstream inputFile(file);
+    Item* itmPtr = myStore.getItem(2505);
+    if(itmPtr != nullptr)
+        cout << itmPtr->getDescription();
+    else
+        cout << "does not exist" << endl;
 
-    // Check if the file is opened successfully
-    if (!inputFile.is_open()) {
-        std::cerr << "We Will Be Closed For Inventory" << std::endl;
-        return 1;  // Exit with an error code
+    /*
+    cout << myStore.getStockList().size() << endl;
+    for (size_t i{0}; i < myStore.getStockList().size(); i++)
+    {
+
+        cout << myStore.getStockList()[i]->getInventoryCode() << endl;
+        cout << myStore.getStockList()[i]->getDescription() << endl;
+        cout << myStore.getStockList()[i]->getPrice() << endl;
+        cout << myStore.getStockList()[i]->getItemWeightedBool() << endl;
     }
-
-    // Process each line in the file
-    std::string line;
-    while (std::getline(inputFile, line)) {
-        std::istringstream input_line(line);
-
-        Item item;
-
-        delimeter_pos = line.find(bar_delimiter);
-        item.code = stoi(line.substr(0,delimeter_pos));
-        line.erase(0,delimeter_pos + 1);
-
-        delimeter_pos = line.find(bar_delimiter);
-        item.description = line.substr(0,delimeter_pos);
-        line.erase(0,delimeter_pos + 1);
-
-        delimeter_pos = line.find(slash_delimiter);
-        if(delimeter_pos != line.npos)
-        {
-            item.price = stof(line.substr(0,delimeter_pos));
-            item.per_pound = true;
-        }else
-        {
-        item.price = stof(line);
-        item.per_pound = false;
-        }
-
-        std::cout << "Code: " << item.code << std::endl;
-        std::cout << "Description: " << item.description << std::endl;
-        std::cout << "Price: " << item.price << std::endl;
-        if(item.per_pound) std::cout << "Per Pound" << std::endl;
-        std::cout << "----------------------" << std::endl;
-    }
-
-    // Close the file
-    inputFile.close();
-
-    return 0; 
+    */
 }
